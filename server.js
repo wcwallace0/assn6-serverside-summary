77c45f390b60cfde886a0bae08f55f244a02e866
Will Wallace <wwallac1@trinity.edu>
2025-04-06 17:01:42 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: false}))
+
+app.set("views", "views");
+app.set("view engine", "pug");
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+app.listen(3018, function() {
+    console.log("Listening on port 3018...");
+});
